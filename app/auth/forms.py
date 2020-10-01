91a38f718aa98e89ae8d433529e330c578454104
David Okwacha <osweredavid@gmail.com>
2020-10-01 14:07:50 +0300

    created views of authentication forms

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -2,7 +2,6 @@ from flask_wtf import FlaskForm
 from wtforms.validators import Required, Email, EqualTo, Length
 from wtforms import ValidationError
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-
 from app.models import User
 
 
