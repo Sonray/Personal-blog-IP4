7bc6736c4c44a80bcf9c9302a75b8ebb64de52d9
David Okwacha <osweredavid@gmail.com>
2020-10-01 15:38:51 +0300

    deleted virtual environment and installed all the dependancies

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,9 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_mail import Mail
-
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-
 # Instances of flask extensions
 # Instance of LoginManger and using its methods
 login_manager = LoginManager()
